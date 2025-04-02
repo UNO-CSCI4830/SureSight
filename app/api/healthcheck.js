ba044e3f189d8bc990f5a0cb9d8314d01c9fbd60
SFaust <95448970+DrFaustest@users.noreply.github.com>
2025-04-02 12:37:46 -0500

    Update healthcheck.js
    Added try catch to help determine the cause of the connection error

--- app/api/healthcheck.js
+++ app/api/healthcheck.js
@@ -1,16 +1,18 @@
 import { supabase } from '../../utils/supabaseClient';
 
 export default async function handler(req, res) {
+    try {
+        const { data, error } = await supabase.rpc('now');
 
-    const { data, error } = await supabase.rpc('now');
-    
-    console.log('Current Timestamp:', data);
-    if (error) {
-        console.error('Error checking database status:', error);
-        console.log('Error:', error.message);
-        res.status(500).json({ success: false, message: 'Error checking database status', error });
-    } else {
-        console.log('Current Timestamp:', data);
-        res.status(200).json({ success: true, data });
+        if (error) {
+            console.error('Error checking database status:', error);
+            res.status(500).json({ success: false, message: 'Error checking database status', error });
+        } else {
+            console.log('Current Timestamp:', data);
+            res.status(200).json({ success: true, data });
+        }
+    } catch (err) {
+        console.error('Unexpected error:', err);
+        res.status(500).json({ success: false, message: 'Unexpected error occurred', error: err.message });
     }
 }
