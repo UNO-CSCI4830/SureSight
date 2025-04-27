d271c34428272688f35b2dec5fc24a4b604697ff
aheftie <crazypenguinboss@gmail.com>
2025-04-27 14:51:16 -0500

    Added top padding and changed input color

--- pages/forgotpassword.tsx
+++ pages/forgotpassword.tsx
@@ -44,7 +44,7 @@ const ForgotPassword: React.FC = () => {
 
 return (
   <Layout title="Forgot Password | SureSight">
-    <div className="max-w-md mx-auto w-full">
+    <div className="max-w-md mx-auto w-full mt-10">
       <div className="text-center mb-8">
         <h1 className="text-3xl font-bold text-gray-900">Forgot Password</h1>
       </div>
@@ -65,6 +65,7 @@ return (
             onChange={(e) => setEmail(e.target.value)}
             required
             placeholder="your-email@example.com"
+            inputClassName='bg-white text-gray-900'
           />
           <button
             type="submit"
